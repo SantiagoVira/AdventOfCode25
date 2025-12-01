86f3eaf6850f2690248f1d668df64c0da3957877
SantiagoVira <santiagovira06@gmail.com>
2025-12-01 00:39:35 -0500

    day 1

--- day01/main.py
+++ day01/main.py
@@ -5,4 +5,16 @@ from helpers import *  # noqa
 # Parse input
 data = parse_input("./input.txt", Parse.lines)
 
-print(data)
+res1, res2 = 0, 0
+dial = 50
+for d in data:
+    num = int(d[1:])
+    sign = -1 if d[0] == "L" else 1
+
+    for i in range(num):
+        dial = (dial + sign) % 100
+        res2 += dial == 0
+
+    res1 += dial == 0
+
+print(res1, res2)
