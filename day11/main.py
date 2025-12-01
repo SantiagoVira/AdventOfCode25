e0a912149cdb0fe1466be83052a079b1d0d76365
SantiagoVira <santiagovira06@gmail.com>
2025-12-01 17:58:47 -0500

    change files

--- day11/main.py
+++ day11/main.py
@@ -4,5 +4,5 @@ from helpers import *  # noqa
 
 # Parse input
 data = parse_input("./input.txt", Parse.lines)
-
-
+ans1 = 0
+ans2 = 0
--- generate-files.ts
+++ generate-files.ts
@@ -12,6 +12,8 @@ from helpers import *  # noqa
 
 # Parse input
 data = parse_input("./input.txt", Parse.lines)
+ans1 = 0
+ans2 = 0
 
 
 `;
